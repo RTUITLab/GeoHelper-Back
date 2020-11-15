0183df62193f0a959c5e9c2a7783be63c2dd09e0
IvLaptev <ivlaptev13@ya.ru>
2020-11-15 15:38:25 +0300

    Fixed file uploading

--- services/GeoHelperAPI/config/app.js
+++ services/GeoHelperAPI/config/app.js
@@ -29,7 +29,8 @@ const express = require('express'),
 			multer = require('multer');
 
 // Connect frontend files
-app.use(express.static('/back/dist'));
+console.log('File storage: /usr/src/app/' + process.env.UPLOAD_DIR);
+app.use('/' + process.env.UPLOAD_DIR, express.static('/usr/src/app/' + process.env.UPLOAD_DIR));
 app.use(bodyParser.json({limit: '25mb'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '25mb' }));
 app.use(multer({ dest: process.env.UPLOAD_DIR }).single("file"));
