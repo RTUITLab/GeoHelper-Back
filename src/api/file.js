202711efab6ce746491ec2b01b12dace05c42a29
ivlaptev <ivlaptev13@ya.ru>
2021-10-28 13:10:14 +0300

    Added files indexing in db

--- src/api/file.js
+++ src/api/file.js
@@ -12,6 +12,16 @@ module.exports = (app) => {
     }
   });
 
+  app.get('/upload', async (req, res) => {
+    try {
+      const files = await fileService.getAllFiles();
+
+      res.status(200).json(files);
+    } catch (e) {
+      res.status(400).json({ success: false, message: e.message });
+    }
+  });
+
   app.delete('/delete_file', Validators.deleteFile, async (req, res) => {
     try {
       await fileService.removeFile(req.body.url);
