3f544bde955310f7bca087beddfb15e4f8703fe2
IvLaptev <ivlaptev13@ya.ru>
2022-04-01 07:26:32 +0300

    Added behaviors list

---
+++ src/config/behavior.js
@@ -0,0 +1,10 @@
+module.exports = {
+  conditions: [
+    'click',
+    'volume'
+  ],
+  actionsTypes: [
+    'excursion',  // Ходьба по маршруту с возвратом
+    'route'       // Ходьба по маршруту
+  ]
+}
